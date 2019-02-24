89ccfd600c3632c3fc493b28e7803cde76a7e38f
Will Smith <minkcv@gmail.com>
2019-02-23 17:04:18 -0700

    exporter

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ var frameSelectElt = document.getElementById('frame-select');
 var frameDurationElt = document.getElementById('frameDuration');
 var frames = [];
 
+// Cube is like a sandwich.
+// 8 layers on top of each other.
+var numRows = 8;
+var numCols = 8;
+var numSlices = 8;
+
 function addFrame() {
     console.log('adding frame');
     var sliceContainerElt = document.createElement('div');
@@ -15,20 +21,17 @@ function addFrame() {
         slices: [],
         elt: sliceContainerElt
     };
-    // Cube is like a sandwich.
-    // 8 layers on top of each other.
-    var numSlices = 8;
+    
     for (var i = 0; i < numSlices; i++) {
         var sliceElt = document.createElement('div');
         sliceElt.className = 'slice';
-        var numRows = 8;
         var colorPickers = [];
         for (var x = 0; x < numRows; x++) {
-            var numCols = 8;
             for (var y = 0; y < numCols; y++) {
                 var colorPickerElt = document.createElement('input');
                 var picker = new jscolor(colorPickerElt);
                 colorPickerElt.className = 'color-box';
+                colorPickerElt.jscolor = picker;
                 picker.fromRGB(0, 0, 0);
                 colorPickers.push(colorPickerElt);
                 sliceElt.appendChild(colorPickerElt);
@@ -55,5 +58,28 @@ function changeFrame() {
         frames[i].elt.style.display = 'none';
     }
     frames[index].elt.style.display = 'block';
+}
+
+function convertToOutput() {
+    var output = '';
+    for (var f = 0; f < frames.length; f++) {
+        for (var s = 0; s < frames[0].slices.length; s++) {
+            var slice = frames[0].slices[s];
+            for (var x = 0; x < numRows; x++) { 
+                for (var y = 0; y < numCols; y++) {
+                    var colorPickerElt = slice.colorPickers[y * numRows + x];
+                    var red = colorPickerElt.jscolor.rgb[0];
+                    var green = colorPickerElt.jscolor.rgb[1];
+                    var blue = colorPickerElt.jscolor.rgb[2];
+                    if (red == 0 && green == 0 && blue == 0)
+                        continue;
+                    var line = 'writeLED(' + (y * numRows + x) + ', ' + red + ', ' + green + ', ' + blue + ');';
+                    output += line + '\n';
+                }
+            }
+        }
+    }
+    var outputElt = document.getElementById('output');
+    outputElt.value = output;
 }
 
